01272b81e089ba2512afef16040c747fc96693c4
AdrizProg <157852821+AdrizProg@users.noreply.github.com>
2024-10-14 16:54:07 +0100

    Tarea 9 finalizada correctamente

--- resources/views/components/community-link.blade.php
+++ resources/views/components/community-link.blade.php
@@ -9,16 +9,16 @@
                     <p class="font-semibold">{{$link->title}}</p>
                     <small class="text-gray-500">Contributed by: {{$link->creator->name}}
                         {{$link->updated_at->diffForHumans()}}</small>
+                        <span class="inline-block px-2 py-1 text-white text-sm font-semibold rounded"
+style="background-color: {{ $link->channel->color }}">
+{{ $link->channel->title }}
+</span>
                 </li>
             @endforeach
         </ul>
         <!-- Paginación de los links -->
         <div class="mt-6">
             {{$links->links()}}
-            <span class="inline-block px-2 py-1 text-white text-sm font-semibold rounded"
-style="background-color: {{ $link->channel->color }}">
-{{ $link->channel->title }}
-</span>
         </div>
     </div>
 </div>
