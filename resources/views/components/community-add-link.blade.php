31cdd4198500821b39e7410d8318861c155113f0
AdrizProg <157852821+AdrizProg@users.noreply.github.com>
2024-10-17 20:01:40 +0100

    Tarea A12 terminada

--- resources/views/components/community-add-link.blade.php
+++ resources/views/components/community-add-link.blade.php
@@ -3,12 +3,23 @@
     <form method="POST" action="/dashboard">
         @csrf
         <div class="mb-4">
-            <label for="title" class="block text-white font-medium" value="{{ old('title') }}">Title:</label>
+            <label for="title" class="block text-white font-medium">Title:</label>
             <input type="text" id="title" name="title"
                 class="mt-1 block w-full rounded-md border-gray-600 bg-gray-700 text-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 @error('title') is-invalid @enderror"
-                placeholder="What is the title of your article?">
+                placeholder="What is the title of your article?" value="{{ old('title') }}">
             @error('title')
-                <div class="alert alert-danger">{{ $message }}</div>
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <span class="block sm:inline">{{ $message }}</span>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 cursor-pointer"
+                        onclick="this.parentElement.style.display='none';">
+                        <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20">
+                            <path
+                                d="M14.348 14.849a1 1 0 01-1.414 0L10 11.414 7.066 14.35a1 1 0 01-1.414-1.414L8.586 10 5.652 7.066a1 1 0 011.414-1.414L10 8.586l2.934-2.934a1 1 0 011.414 1.414L11.414 10l2.934 2.934a1 1 0 010 1.415z" />
+                        </svg>
+                    </span>
+                </div>
+
             @enderror
         </div>
 
@@ -16,9 +27,20 @@
             <label for="link" class="block text-white font-medium">Link:</label>
             <input type="text" id="link" name="link"
                 class="mt-1 block w-full rounded-md border-gray-600 bg-gray-700 text-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 @error('link') is-invalid @else is-valid @enderror"
-                placeholder="What is the URL?">
+                placeholder="What is the URL?" value="{{ old('link') }}">
             @error('link')
-                <div class="alert alert-danger">{{ $message }}</div>
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <span class="block sm:inline">{{ $message }}</span>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3 cursor-pointer"
+                        onclick="this.parentElement.style.display='none';">
+                        <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20">
+                            <path
+                                d="M14.348 14.849a1 1 0 01-1.414 0L10 11.414 7.066 14.35a1 1 0 01-1.414-1.414L8.586 10 5.652 7.066a1 1 0 011.414-1.414L10 8.586l2.934-2.934a1 1 0 011.414 1.414L11.414 10l2.934 2.934a1 1 0 010 1.415z" />
+                        </svg>
+                    </span>
+                </div>
+
             @enderror
         </div>
 
